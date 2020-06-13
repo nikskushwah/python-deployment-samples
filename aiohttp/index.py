ae717a5d87ede56e96b1d700455782a0ec5ac1f7
Edison Garcia Chinas <edisga@microsoft.com>
2020-06-13 12:51:51 -0500

    Adding aiohttp

---
+++ aiohttp/index.py
@@ -0,0 +1,20 @@
+from aiohttp import web
+
+async def handle(request):
+    name = request.match_info.get('name', "Anonymous")
+    text = "Hello, " + name
+    return web.Response(text=text)
+
+app = web.Application()
+app.add_routes([web.get('/', handle),
+                web.get('/{name}', handle)])
+
+if __name__ == '__main__':
+    web.run_app(app)
+
+## If you want to run in development over TCP/IP
+def init_func(argv):
+    app = web.Application()
+    app.add_routes([web.get('/', handle),
+                    web.get('/{name}', handle)])
+    return app
